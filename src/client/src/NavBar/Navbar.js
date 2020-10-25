7f33a5f7658e249c27a3ca4070056d91be5b2138
shaj684 <48861103+shaj684@users.noreply.github.com>
2020-10-25 03:37:31 -0400

    changing navBar

--- src/client/src/NavBar/Navbar.js
+++ src/client/src/NavBar/Navbar.js
@@ -13,7 +13,7 @@ class Navbar extends Component {
   render(){ 
     return( 
       <nav className="NavbarItems">
-         <h1 className="navbar-logo"> Spring <i className="fab fa-react"></i></h1> 
+         <h1 className="navbar-logo"> Spring <img src="../../public/spring.png"></img> </h1> 
            <div className="menu-icon" onClick={this.handleClick}><i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i> </div>
             <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
               {MenuItems.map((item, index) => { 
