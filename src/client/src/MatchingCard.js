c089a6abbd60917f7c07e2a32396ab91baf8ef17
shaj684 <48861103+shaj684@users.noreply.github.com>
2020-10-25 04:45:44 -0400

    adding matching card

---
+++ src/client/src/MatchingCard.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+function MatchingCard() {
+  
+    return( 
+      <div>Test</div>
+    )
+
+}
+
+export default MatchingCard;
